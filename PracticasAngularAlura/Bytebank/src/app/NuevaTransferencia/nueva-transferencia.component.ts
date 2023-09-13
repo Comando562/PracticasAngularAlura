54f302ec06abb19fda8f91642f8e6f4ee102c9c8
Leonardo Moya <leo.moya562@gmail.com>
2023-09-12 23:47:29 -0600

    Segundo Commit | primera clase

---
+++ PracticasAngularAlura/Bytebank/src/app/NuevaTransferencia/nueva-transferencia.component.ts
@@ -0,0 +1,10 @@
+//AQUI VAMOS ADEFINIR LA FUNCIONALIDAD DEL COMPONENTE
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nueva-transferencia',   //Asi vamos a querer llamar a nuestro componente
+    templateUrl: './nueva-transferencia.component.html', //Aqui le decimos en donde va  a obtener la estructura de nuestro componente
+    styleUrls: ['./nueva-transferencia.component.scss']
+})
+export class NuevaTransferenciaComponent { }
--- PracticasAngularAlura/Bytebank/src/app/app.module.ts
+++ PracticasAngularAlura/Bytebank/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NuevaTransferenciaComponent } from './NuevaTransferencia/nueva-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevaTransferenciaComponent
   ],
   imports: [
     BrowserModule
